6daec9ae69c592479020539190858dfe27543713
Shinya TAKEUCHI / 武内 晋哉 <60569929+shinya-tokyo@users.noreply.github.com>
2023-08-25 14:49:24 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
 #include <M5Core2.h>
 #include <stdio.h>
-// #include <string.h>
 #include <string>
 
 #define SERVO_PIN  32
 #define SERVO_CH   0
 #define PWM_HZ 50
-#define PWM_BIT 16 // 16bit(0～65535)
+#define PWM_BIT 16
 
-#define MAX 7800 // 65535 / 20 * 2.4 ≒ 7864 
-#define MIN 1700 // 65535 / 20 * 0.5 ≒ 1638
+#define MAX 7800
+#define MIN 1700
 
 void setup() {
 
-    M5.begin(true, true, true, false); // I2C無効(PORTA をGPIOで使用するため)
+    M5.begin(true, true, true, false);
 
     ledcSetup(SERVO_CH, PWM_HZ, PWM_BIT);
     ledcAttachPin(SERVO_PIN, SERVO_CH);
@@ -31,9 +30,7 @@ void motor(int angle) {
     ledcWrite(SERVO_CH, duty_cycle);
 
     M5.Lcd.fillScreen(BLACK);
-    // M5.Lcd.setCursor(80, 80);
     M5.Lcd.setCursor(10, 10);
-    // sprintf(str,"%3d", angle);
     M5.Lcd.setTextColor(GREEN);
 
     if((angle >= 0) && (angle <= 45)){
@@ -56,8 +53,6 @@ void motor(int angle) {
         str = "HI: " + std::to_string(angle) + "\nCCW";
         // str = "HI\nCCW";       
     }
-
-    // M5.Lcd.drawCentreString(str.c_str(), 150, 70, 7);
     M5.Lcd.print(str.c_str());
 }
 
@@ -87,20 +82,4 @@ void loop() {
 
     motor(90); // Stop
     delay(1500);
- 
-
-    // delay(1000);    
-
-    /*
-    for (int angle = 0; angle <= 180; angle += 1) {
-        int duty_cycle  = map(angle, 0, 180, MIN, MAX); 
-        ledcWrite(SERVO_CH, duty_cycle);
-
-        M5.Lcd.fillScreen(BLACK);
-        M5.Lcd.setCursor(80, 80);
-        sprintf(str,"%3d", angle);
-        M5.Lcd.drawCentreString(str, 150, 70, 7);
-        delay(1000);
-    }
-    */
 }
